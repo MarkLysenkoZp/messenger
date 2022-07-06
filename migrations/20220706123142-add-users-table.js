6bd96769225850e5bd7ae08f3f14a65ac5bc89fd
MacBook <ilya.nechiporenko@gmail.com>
2022-07-06 15:52:16 +0300

    To add migration for users table

---
+++ migrations/20220706123142-add-users-table.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db, callback) {
+  db.createTable('users', {
+    columns: {
+      id: { type: 'bigint', primaryKey: true, autoIncrement: true },
+      email: 'string',
+      password: 'string',
+      nickname: 'string',
+      phone: 'string'
+    },
+    ifNotExists: true
+  }, callback);
+};
+
+exports.down = function(db, callback) {
+  db.dropTable('users', { ifExists: true }, callback);
+};
+
+exports._meta = {
+  "version": 1
+};
