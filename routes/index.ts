5c3dc802208c802669d65476c1030cd964878999
MacBook <ilya.nechiporenko@gmail.com>
2022-07-16 11:20:15 +0300

    Preparations to implement authentication

--- routes/index.ts
+++ routes/index.ts
@@ -1,13 +1,13 @@
 import express, { Express, Request, Response, Router } from 'express';
 const app: Express = express();
-const router: Router = express.Router();
+const indexRouter: Router = express.Router();
 import testConnection from '../dbConnection';
 
 /* GET users listing. */
-router.get('/', async  (req: Request, res: Response) => {
+indexRouter.get('/', async  (req: Request, res: Response) => {
   // Test DB connection
   await testConnection();
   res.render('index');
 });
 
-export default router;
+export default indexRouter;
