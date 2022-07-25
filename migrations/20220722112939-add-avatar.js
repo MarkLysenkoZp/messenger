3cc57dfb33e68fa365d4c48faa22a6b851614461
MarkLysenkoZp <marcuslysenko061@gmail.com>
2022-07-25 15:26:11 +0300

    migration for avatar

--- migrations/20220722112939-add-avatar.js
+++ migrations/20220722112939-add-avatar.js
@@ -8,23 +8,21 @@ var seed;
   * We receive the dbmigrate dependency from dbmigrate initially.
   * This enables us to not have to rely on NODE_PATH.
   */
+
 exports.setup = function(options, seedLink) {
   dbm = options.dbmigrate;
   type = dbm.dataType;
   seed = seedLink;
 };
 
-const User = require('../dist/models/User.js').default;
-
 exports.up = function(db, callback) {
-  User.sync({ force: true });
-  callback();
-  
+  db.addColumn( 'users', 'avatar', { type: 'string' }, callback() );
 };
 
 exports.down = function(db, callback) {
-  db.dropTable('users', { ifExists: true }, callback);
+  db.removeColumn('users', 'avatar', callback);
 };
+
 
 exports._meta = {
   "version": 1
