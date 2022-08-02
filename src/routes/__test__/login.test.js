da15f9d0deb0a6b14ff1c69f6e04a0f287c3316a
MarkLysenkoZp <marcuslysenko061@gmail.com>
2022-08-02 18:17:17 +0300

    Logout

--- src/routes/__test__/login.test.js
+++ src/routes/__test__/login.test.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const request = require("supertest");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const server = require("../../../dist/server").default;
 
 afterEach(() => {
