2423d257f5c3ede3e8002cf3e152c44e21cb89a0
MarkLysenkoZp <98688635+MarkLysenkoZp@users.noreply.github.com>
2022-08-04 00:39:19 +0300

    [#182889277] Добавить возможность удалять текущего пользователя (#17)
    * delete
    
    * Error correctiom
    
    * Error correctiom
    
    * Error correctiom
    
    * Error correctiom
    
    * Error correctiom
    
    Co-authored-by: Ilya <ilya.nechiporenko@gmail.com>

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,14 +2,10 @@ import express, { Express, Request, Response, Router } from 'express';
 const app: Express = express();
 const indexRouter: Router = express.Router();
 import { testConnection } from '../dbConnection';
-
 import { auth } from '../middleware/auth';
 
-/* GET users listing. */
 indexRouter.get('/', auth, async  (req: Request, res: Response) => {
-  // Test DB connection
   await testConnection();
   res.render('index');
 });
-
 export default indexRouter;
