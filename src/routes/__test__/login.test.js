cc35f3f790551726311ff972ef1d3e941af88f1c
MarkLysenkoZp <marcuslysenko061@gmail.com>
2022-08-09 18:00:27 +0300

    unittest for success massage

--- src/routes/__test__/login.test.js
+++ src/routes/__test__/login.test.js
@@ -23,7 +23,11 @@ describe('login router', () => {
     expect(response.statusCode).toBe(200);
   });
 
-  test('renders a message about successful registration', () => {
-    // TODO
+  test('renders a message about successful registration', async () => {
+    const success = 'You have been registered successfully. Please, login to continue';
+    const response = await request(server).get("/login?success=ok");
+    console.log('Text',response.text);
+    expect(response.text.includes(success)).toEqual(true);
+    expect(response.statusCode).toBe(200);
   })
 });
